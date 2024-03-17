9f29ce41a6cfd512b910ced8ba603db1961e24ec
DianQK <dianqk@dianqk.net>
2024-03-17 16:49:53 +0800

    [InlineCost] Reduce a comparison

--- llvm/lib/Analysis/InlineCost.cpp
+++ llvm/lib/Analysis/InlineCost.cpp
@@ -536,8 +536,16 @@ public:
 // Considering comparisons from leaf and non-leaf nodes, we can estimate the
 // number of comparisons in a simple closed form :
 //   n + n / 2 - 1 = n * 3 / 2 - 1
-int64_t getExpectedNumberOfCompare(int NumCaseCluster) {
-  return 3 * static_cast<int64_t>(NumCaseCluster) / 2 - 1;
+int64_t getExpectedNumberOfCompare(int NumCaseCluster,
+                                   bool DefaultDestUndefined) {
+  int64_t ExpectedNumber = 3 * static_cast<int64_t>(NumCaseCluster) / 2 - 1;
+  // FIXME: The compare instruction count should be less than the branch count
+  // when default branch is undefined. But this will cause some performance
+  // regressions. At least, we can now try to remove a compare instruction.
+  if (DefaultDestUndefined) {
+    ExpectedNumber -= 1;
+  }
+  return ExpectedNumber;
 }
 
 /// FIXME: if it is necessary to derive from InlineCostCallAnalyzer, note
@@ -724,9 +732,8 @@ class InlineCostCallAnalyzer final : public CallAnalyzer {
       return;
     }
 
-    // FIXME: Consider the case when default branch is undefined.
     int64_t ExpectedNumberOfCompare =
-        getExpectedNumberOfCompare(NumCaseCluster);
+        getExpectedNumberOfCompare(NumCaseCluster, DefaultDestUndefined);
     int64_t SwitchCost = ExpectedNumberOfCompare * 2 * InstrCost;
 
     addCost(SwitchCost);
@@ -1258,9 +1265,8 @@ private:
       return;
     }
 
-    // FIXME: Consider the case when default branch is undefined.
     int64_t ExpectedNumberOfCompare =
-        getExpectedNumberOfCompare(NumCaseCluster);
+        getExpectedNumberOfCompare(NumCaseCluster, DefaultDestUndefined);
 
     int64_t SwitchCost =
         ExpectedNumberOfCompare * SwitchCostMultiplier * InstrCost;
