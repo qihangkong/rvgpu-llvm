dc2c2faa82d3d7b998680267a79895eb4969e6fd
DianQK <dianqk@dianqk.net>
2024-03-17 18:05:01 +0800

    [perf experiment] Update the number of comparisons for the default branch

--- llvm/lib/Analysis/InlineCost.cpp
+++ llvm/lib/Analysis/InlineCost.cpp
@@ -538,14 +538,12 @@ public:
 //   n + n / 2 - 1 = n * 3 / 2 - 1
 int64_t getExpectedNumberOfCompare(int NumCaseCluster,
                                    bool DefaultDestUndefined) {
-  int64_t ExpectedNumber = 3 * static_cast<int64_t>(NumCaseCluster) / 2 - 1;
-  // FIXME: The compare instruction count should be less than the branch count
-  // when default branch is undefined. But this will cause some performance
-  // regressions. At least, we can now try to remove a compare instruction.
+  // The compare instruction count should be less than the branch count
+  // when default branch is undefined.
   if (DefaultDestUndefined) {
-    ExpectedNumber -= 1;
+    return static_cast<int64_t>(NumCaseCluster) - 1;
   }
-  return ExpectedNumber;
+  return 3 * static_cast<int64_t>(NumCaseCluster) / 2 - 1;
 }
 
 /// FIXME: if it is necessary to derive from InlineCostCallAnalyzer, note
