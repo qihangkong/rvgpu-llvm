abfb36b0b8b958d0c6e63f73bd242e27e1d11a80
Xiaoyang Liu <siujoeng.lau@gmail.com>
2024-03-16 23:37:44 -0700

    [libc++][ranges] optimize 'ranges::starts_with' for 'sized_range'

--- libcxx/include/__algorithm/ranges_starts_with.h
+++ libcxx/include/__algorithm/ranges_starts_with.h
@@ -21,6 +21,7 @@
 #include <__iterator/next.h>
 #include <__ranges/access.h>
 #include <__ranges/concepts.h>
+#include <__ranges/size.h>
 #include <__utility/move.h>
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
@@ -37,15 +38,22 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 namespace ranges {
 namespace __starts_with {
 struct __fn {
-  template <class _Iter1, class _Sent1, class _Iter2, class _Sent2, class _Pred, class _Proj1, class _Proj2>
-  _LIBCPP_NODISCARD_EXT _LIBCPP_HIDE_FROM_ABI static constexpr bool __starts_with_impl(
+  template <input_iterator _Iter1,
+            sentinel_for<_Iter1> _Sent1,
+            input_iterator _Iter2,
+            sentinel_for<_Iter2> _Sent2,
+            class _Pred  = ranges::equal_to,
+            class _Proj1 = identity,
+            class _Proj2 = identity>
+    requires indirectly_comparable<_Iter1, _Iter2, _Pred, _Proj1, _Proj2>
+  _LIBCPP_NODISCARD_EXT _LIBCPP_HIDE_FROM_ABI static constexpr bool operator()(
       _Iter1 __first1,
       _Sent1 __last1,
       _Iter2 __first2,
       _Sent2 __last2,
-      _Pred& __pred,
-      _Proj1& __proj1,
-      _Proj2& __proj2) {
+      _Pred __pred   = {},
+      _Proj1 __proj1 = {},
+      _Proj2 __proj2 = {}) {
     if constexpr (sized_sentinel_for<_Sent1, _Iter1> && sized_sentinel_for<_Sent2, _Iter2>) {
       auto __n1 = ranges::distance(__first1, __last1);
       auto __n2 = ranges::distance(__first2, __last2);
@@ -56,7 +64,7 @@ struct __fn {
         return false;
       }
 
-      if constexpr (random_access_iterator<_Iter1> && random_access_iterator<_Iter2>) {
+      if constexpr (contiguous_iterator<_Iter1> && contiguous_iterator<_Iter2>) {
         return ranges::equal(
             std::move(__first1),
             ranges::next(__first1, __n2),
@@ -79,26 +87,6 @@ struct __fn {
                .in2 == __last2;
   }
 
-  template <input_iterator _Iter1,
-            sentinel_for<_Iter1> _Sent1,
-            input_iterator _Iter2,
-            sentinel_for<_Iter2> _Sent2,
-            class _Pred  = ranges::equal_to,
-            class _Proj1 = identity,
-            class _Proj2 = identity>
-    requires indirectly_comparable<_Iter1, _Iter2, _Pred, _Proj1, _Proj2>
-  _LIBCPP_NODISCARD_EXT _LIBCPP_HIDE_FROM_ABI static constexpr bool operator()(
-      _Iter1 __first1,
-      _Sent1 __last1,
-      _Iter2 __first2,
-      _Sent2 __last2,
-      _Pred __pred   = {},
-      _Proj1 __proj1 = {},
-      _Proj2 __proj2 = {}) {
-    return __starts_with_impl(
-        std::move(__first1), std::move(__last1), std::move(__first2), std::move(__last2), __pred, __proj1, __proj2);
-  }
-
   template <input_range _Range1,
             input_range _Range2,
             class _Pred  = ranges::equal_to,
@@ -107,14 +95,37 @@ struct __fn {
     requires indirectly_comparable<iterator_t<_Range1>, iterator_t<_Range2>, _Pred, _Proj1, _Proj2>
   _LIBCPP_NODISCARD_EXT _LIBCPP_HIDE_FROM_ABI static constexpr bool
   operator()(_Range1&& __range1, _Range2&& __range2, _Pred __pred = {}, _Proj1 __proj1 = {}, _Proj2 __proj2 = {}) {
-    return __starts_with_impl(
-        ranges::begin(__range1),
-        ranges::end(__range1),
-        ranges::begin(__range2),
-        ranges::end(__range2),
-        __pred,
-        __proj1,
-        __proj2);
+    if constexpr (sized_range<_Range1> && sized_range<_Range2>) {
+      auto __n1 = ranges::size(__range1);
+      auto __n2 = ranges::size(__range2);
+      if (__n2 == 0) {
+        return true;
+      }
+      if (__n2 > __n1) {
+        return false;
+      }
+
+      if constexpr (contiguous_range<_Range1> && contiguous_range<_Range2>) {
+        return ranges::equal(
+            ranges::begin(__range1),
+            ranges::next(ranges::begin(__range1), __n2),
+            ranges::begin(__range2),
+            ranges::end(__range2),
+            std::ref(__pred),
+            std::ref(__proj1),
+            std::ref(__proj2));
+      }
+    }
+
+    return ranges::mismatch(
+               ranges::begin(__range1),
+               ranges::end(__range1),
+               ranges::begin(__range2),
+               ranges::end(__range2),
+               std::ref(__pred),
+               std::ref(__proj1),
+               std::ref(__proj2))
+               .in2 == ranges::end(__range2);
   }
 };
 } // namespace __starts_with
