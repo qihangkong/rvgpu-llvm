480457e63da40fab885d401d5c7f7258a00829e3
Thorsten Schütt <schuett@gmail.com>
2024-03-30 08:08:26 +0100

    simplify

--- llvm/lib/CodeGen/GlobalISel/CombinerHelper.cpp
+++ llvm/lib/CodeGen/GlobalISel/CombinerHelper.cpp
@@ -7150,7 +7150,6 @@ bool CombinerHelper::matchAddOverflow(MachineInstr &MI, BuildFnTy &MatchInfo) {
 void CombinerHelper::applyBuildFnMO(const MachineOperand &MO,
                                     BuildFnTy &MatchInfo) {
   MachineInstr *Root = getDefIgnoringCopies(MO.getReg(), MRI);
-  Builder.setInstrAndDebugLoc(*Root);
   MatchInfo(Builder);
   Root->eraseFromParent();
 }
@@ -7163,10 +7162,6 @@ bool CombinerHelper::matchSextOfTrunc(const MachineOperand &MO,
 
   GTrunc *Trunc = getOpcodeDef<GTrunc>(Sext->getSrcReg(), MRI);
   if (!Trunc)
-    return false;
-
-  // The trunc must have the nsw flag.
-  if (!Trunc->getFlag(MachineInstr::MIFlag::NoSWrap))
     return false;
 
   Register Dst = Sext->getReg(0);
@@ -7192,10 +7187,6 @@ bool CombinerHelper::matchZextOfTrunc(const MachineOperand &MO,
 
   GTrunc *Trunc = getOpcodeDef<GTrunc>(Zext->getSrcReg(), MRI);
   if (!Trunc)
-    return false;
-
-  // The trunc must have the nuw flag.
-  if (!Trunc->getFlag(MachineInstr::MIFlag::NoUWrap))
     return false;
 
   Register Dst = Zext->getReg(0);
